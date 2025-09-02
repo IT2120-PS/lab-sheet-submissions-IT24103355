189b9fd0b25ce74b1923d7bdd4353caf31031332
IT24103355 <it24103355@my.sliit.lk>
2025-09-02 23:06:41 +0530

    Lab6

---
+++ Lab Sheet 06/it24103355.R
@@ -0,0 +1,12 @@
+setwd("C:\\Users\\Admin\\Desktop\\IT24103355 Lab5")
+dbinom(40,44,0.92)
+pbinom(35, 44, 0.92,lower.tail=TRUE)
+1 - pbinom(37, 44, 0.92,lower.tail = TRUE)
+pbinom(37, 44, 0.92, lower.tail = FALSE)
+pbinom(42, 44, 0.92,lower.tail = TRUE)-pbinom(39, 44, 0.92, lower.tail = TRUE)
+dpois(6,5)
+ppois(6,5,lower.tail = FALSE)
+
+1-pbinom(46, 50, 0.85, lower.tail = TRUE)
+
+dpois(15,12)
